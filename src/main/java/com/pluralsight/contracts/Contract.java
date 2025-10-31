3431913ac03efe3754b4e3df06cddbaef035646e
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-10-31 13:21:41 -0400

    feat(contracts): add abstract Contract with common fields

---
+++ src/main/java/com/pluralsight/contracts/Contract.java
@@ -0,0 +1,35 @@
+package com.pluralsight.contracts;
+
+import com.pluralsight.model.Car;
+
+public abstract class Contract {
+    private final String date;
+    private final String customerName;
+    private final String customerEmail;
+    private  final Car car;
+
+    protected Contract(String date, String customerName, String customerEmail, Car car) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.car = car;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getCustomerEmail() {
+        return getCustomerEmail;
+    }
+    public Car getCar() {
+        return car;
+    }
+
+    public  abstract  double getTotalPrice();
+
+    public abstract  double getMonthlyPayment();
+
+}
